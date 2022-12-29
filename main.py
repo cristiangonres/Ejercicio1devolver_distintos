57d6e5f044523be59e0d62777911bf2d24afad66
cristian <gonzalez.resina@gmail.com>
2022-12-29 13:51:32 +0100

    Comentario con enunciado de ejercicio

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+"""Crea una función llamada devolver_distintos() que reciba 3
+integers como parámetros.
+Si la suma de los 3 numeros es mayor a 15, va a devolver el
+número mayor.
+Si la suma de los 3 numeros es menor a 10, va a devolver el
+número menor.
+Si la suma de los 3 números es un valor entre 10 y 15
+(incluidos) va a devolver el número de valorintermedio."""
+
 def devolver_distintos(*args):
     total = sum(args)
     if total > 15:
